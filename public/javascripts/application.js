c749fd5687ee72e4835b6815204cac6f8c9e2619
Dojo User <dojouser@Obtiva-Coding-Dojo-3.local>
2010-09-11 16:47:32 -0500

    Highlight message when user's name is mentioned

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -41,12 +41,16 @@
        $('#last_sync').text(data['time']);
        $(data['delta']).each(function() {
          var message = this;
-         console.log(message);
          var chatMessageTemplate = '<div class="chat_message clearfix"><span class="identity"><img src="{{gravatar_url}}" /></span><span class="message">({{posted_at}}) <b class="username">{{username}}</b> {{{message}}}</span></div>';
 
          //var chatMessage = "<img src=\"" + message["gravatar_url"] + "\" class=\"gravatar\" /><b>" + message["username"] + "</b> " + message["message"];
          var chatMessage = Mustache.to_html(chatMessageTemplate, message);
          var chatElement = $(chatMessage);
+         
+          if (message.message.match($.cookie('username'))) {
+  		      chatElement.addClass("me");
+  	      }
+         
          if(! WCR.odd) {
          chatElement.addClass("odd");
          WCR.odd = true;
